b42743b9cd722a0753486bb8aa90dce8ee646cfa
Mon3ta <rescobarmo@gmail.com>
2022-04-12 09:19:00 -0400

    12042022

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,18 +1,98 @@
-const obtenerProyectos = async (req, res) => {};
+import Proyecto from "../models/Proyectos.js";
 
-const nuevoProyecto = async (req, res) => {
+const obtenerProyectos = async (req, res) => {
+    const proyecto = await Proyecto.find().where("creador").equal(req.usuario);
 
-    console.log(req.body);
-    console.log(req.usuario);
+    res.json(proyecto);
 
 
 };
 
-const obtenerProyecto = async (req, res) => {};
+const nuevoProyecto = async (req, res) => {
 
-const editarProyecto = async (req, res) => {};
+    const proyecto = new Proyecto(req.body)
+    proyecto.creador = req.usuario._id;
 
-const eliminarProyecto = async (req, res) => {};
+    try {
+        const proyectoAlacenado = await proyecto.save();
+        res.json(proyectoAlacenado)
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+};
+
+const obtenerProyecto = async (req, res) => {
+    const { id } = req.params;// leer el proyecto   
+    const proyecto = await Proyecto.findById(id); // leer el proyecto   
+
+    if (!proyecto){ //verifico que existe 
+        const error = new Error ("No Encontrado")
+        return res.status(404).json({msg: error.message})
+    } 
+
+    if (proyecto.creador.toString() !== req.usuario._id.toString() ){ //verifica que la persona quew lo creo ahara las modificacioones 
+        const error = new Error ("No Accion no Valida")
+        return res.status(404).json({msg: error.message})
+    }
+
+    res.json(proyecto)
+};
+
+const editarProyecto = async (req, res) => {
+    const { id } = req.params;// leer el proyecto   
+    const proyecto = await Proyecto.findById(id); // leer el proyecto   
+
+    if (!proyecto){ //verifico que existe 
+        const error = new Error ("No Encontrado")
+        return res.status(404).json({msg: error.message})
+    } 
+
+    if (proyecto.creador.toString() !== req.usuario._id.toString() ){ //verifica que la persona quew lo creo ahara las modificacioones 
+        const error = new Error ("No Accion no Valida")
+        return res.status(404).json({msg: error.message})
+    }
+
+    proyecto.nombre = req.body.nombre ||  proyecto.nombre;
+    proyecto.descripcion = req.body.descripcion ||  proyecto.descripcion;
+    proyecto.fechaEntrega = req.body.fechaEntrega ||  proyecto.fechaEntrega;
+    proyecto.clientes = req.body.clientes ||  proyecto.clientes;
+
+    try {
+        const proyectoAlacenado = await proyecto.save();
+        res.json(proyectoAlacenado)
+    } catch (error) {
+        console.log(error)
+    }
+
+};
+
+const eliminarProyecto = async (req, res) => {
+
+    const { id } = req.params;// leer el proyecto   
+    const proyecto = await Proyecto.findById(id); // leer el proyecto   
+
+    if (!proyecto){ //verifico que existe 
+        const error = new Error ("No Encontrado")
+        return res.status(404).json({msg: error.message})
+    } 
+
+    if (proyecto.creador.toString() !== req.usuario._id.toString() ){ //verifica que la persona quew lo creo ahara las modificacioones 
+        const error = new Error ("No Accion no Valida")
+        return res.status(404).json({msg: error.message})
+    }
+
+    try {
+        await proyecto.deleteOne();
+        res.json({msg:"Proyecto Eliminado"})
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+};
 
 const agregarColaborador = async (req, res) => {};
 
